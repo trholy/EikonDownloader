45d35f83de9392ef71b524d87677281cae858cc4
Thomas R. Holy <thomas.holy@eah-jena.de>
2025-04-04 13:35:34 +0200

    Added setup files.

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="eikondownloader",
+    version="0.1.0",
+    author="Thomas R. Holy",
+    author_email="thomas.robert.holy@gmail.com",
+    description="Download data from Refinitiv Eikon",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/trholy/eikondownloader",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    install_requires=[
+        "minio>=7.0.0",
+        "numpy>=1.24.3",
+        "pandas>=1.5.2",
+        "eikon>=1.1.18",
+    ],
+    extras_require={
+        "dev": [
+            "pytest",
+            "ruff"
+        ]
+    },
+    test_suite='pytest',
+)
