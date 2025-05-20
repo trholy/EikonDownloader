90c93645a3f8197be5a6e885c27f68643f7e011c
Thomas R. Holy <thomas.holy@eah-jena.de>
2025-05-20 08:36:45 +0200

    Applied ruff fixes on imports.

--- src/eikondownloader/__init__.py
+++ src/eikondownloader/__init__.py
@@ -2,5 +2,5 @@ __version__ = "0.1.0"
 __author__ = "Thomas R. Holy"
 
 from .download import EikonDownloader, OSDownloader
-from .upload import OSUploader
 from .processing import DataProcessor
+from .upload import OSUploader
--- src/eikondownloader/processing/processor.py
+++ src/eikondownloader/processing/processor.py
@@ -1,11 +1,10 @@
-import pandas as pd
-import numpy as np
-
-from typing import Union
-import logging
 import glob
+import logging
 import os
+from typing import Union
 
+import numpy as np
+import pandas as pd
 
 # Configure logging to remove the default prefix
 logging.basicConfig(
